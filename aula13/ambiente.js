fc940e8fc0fef1fb17bda93011337aac8d773ee8
Leonardo Cunha <121736215+DevLeoCunha@users.noreply.github.com>
2023-03-09 18:29:46 -0300

    Estrutura de Repetição while

---
+++ aula13/ambiente.js
@@ -0,0 +1,23 @@
+var c = 1
+while (c <= 6) {
+    console.log(`Passo ${c}`)
+    c++ // c = c+1
+}
+
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++ // c = c+1
+} while (c <= 6)
+
+//As duas essencialmente fazem a mesma coisa, a primeira faz o teste primeiro e a segunda faz o teste por ultimo.
+
+
+
+
+
+
+
+
+/*
+console.log('Tudo bem?')*/
